3448913b21ca902af65e710005383372e49be69e
Maurice Wipf <maurice@gmx.de>
2025-05-16 10:46:03 +0200

    Log postgres queries

--- lib/db/queries.ts
+++ lib/db/queries.ts
@@ -38,7 +38,11 @@ import type { VisibilityType } from '@/components/visibility-selector';
 // https://authjs.dev/reference/adapter/drizzle
 
 // biome-ignore lint: Forbidden non-null assertion.
-const client = postgres(process.env.POSTGRES_URL!);
+const client = postgres(process.env.POSTGRES_URL!, {
+  debug: (connection, query, parameters) => {
+    console.log('Executing query:', query, parameters);
+  }
+});
 const db = drizzle(client);
 
 export async function getUser(email: string): Promise<Array<User>> {
